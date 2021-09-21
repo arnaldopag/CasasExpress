0608536d449ed58e635882d67ea3a302992438b4
arnaldopag <arnaldopaganijr@gmail.com>
2021-09-21 14:50:38 -0300

    exercicio pronto

--- ex-i/index.js
+++ ex-i/index.js
@@ -4,7 +4,25 @@ const app = express();
 const port = 3000;
 
 app.get("/casas", function (req, res) {
-    res.json(dados);
+    let consulta = req.query.consulta;
+    let retorno = [];
+    if (consulta) {
+        retorno = dados.filter(function (item) {
+            const name = item.name.toLowerCase();
+            return name.includes(consulta.toLowerCase());
+        });
+    }
+    res.json(retorno);
+});
+app.get("/casas/:slug", function (req, res) {
+    let slug = req.params.slug;
+    let retorno = [];
+    if (slug) {
+        retorno = dados.filter(function (item) {
+            return item.slug == slug;
+        });
+    }
+    res.json(retorno);
 });
 
 app.listen(port, function () {
